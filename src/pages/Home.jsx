ff866fd30ed92de29459a4dab757056af05d83be
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-13 14:46:58 +0530

    design navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
